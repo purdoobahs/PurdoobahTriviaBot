d43318d5e9bbe185035ce2606f0fac38d4d74457
cmcgugan85 <cmcgugan@purdue.edu>
2020-05-02 21:24:47 -0400

    basic SQLite and a few commands

---
+++ sql.py
@@ -0,0 +1,32 @@
+import sqlite3
+import pandas
+
+conn = sqlite3.connect('trivia.db')
+
+c = conn.cursor()
+
+# c.execute("""CREATE TABLE players (
+#     name text,
+#     userID integer
+#     )""")
+
+
+def addPlayer(incominguser, incominguserID):
+    conn, c = connectDB()
+    c.execute("""SELECT * FROM players WHERE name = ?""",(incominguser,))
+    list = c.fetchall()
+    if(len(list) != 0):
+        return -1,list[0]
+    c.execute("""SELECT name FROM players WHERE userID = ?""",(incominguserID,))
+    list = c.fetchall()
+    if(len(list) != 0):
+        return -2, list[0]
+    c.execute("INSERT INTO players VALUES (?,?)", (incominguser, incominguserID))
+    conn.commit()
+    conn.close()
+    return
+
+def connectDB():
+    conn = sqlite3.connect('trivia.db')
+    c = conn.cursor()
+    return conn,c
